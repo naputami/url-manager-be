f4fb652d0a5b7c2f4d3e0e61cf3cb1e0d0e3ee22
Nadia Puji Utami <nadiapujiutami@gmail.com>
2024-11-09 20:42:50 +0700

    fix(commitlint): edit rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,7 +4,7 @@ export default {
     "type-enum": [
       2,
       "always",
-      ["feat", "fix", "docs", "style", "refactor", "test", "revert"],
+      ["feat", "fix", "docs", "style", "refactor", "test", "revert", "chore"],
     ],
   },
 };

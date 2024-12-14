5af72e7611b94dce9300b949833d4309b52b42ac
Nadia Puji Utami <nadiapujiutami@gmail.com>
2024-12-14 22:31:34 +0700

    chore: prettier category interface

--- src/infrastructure/interfaces/category.ts
+++ src/infrastructure/interfaces/category.ts
@@ -1,9 +1,12 @@
-import { Category} from "@prisma/client";
+import { Category } from "@prisma/client";
 
 export interface ICategory {
-    getCategoriesByUser: (userId: string, name: string | undefined) => Promise<Partial<Category>[]>;
-    createCategory: (name: string, userId: string) => Promise<Category>;
-    updateCategory: (name: string, id: string) => Promise<Category | null>;
-    deleteCategory: (id: string) => Promise<Category | null>;
-    getCategoryById: (id: string) => Promise<Category | null>;
+  getCategoriesByUser: (
+    userId: string,
+    name: string | undefined
+  ) => Promise<Partial<Category>[]>;
+  createCategory: (name: string, userId: string) => Promise<Category>;
+  updateCategory: (name: string, id: string) => Promise<Category | null>;
+  deleteCategory: (id: string) => Promise<Category | null>;
+  getCategoryById: (id: string) => Promise<Category | null>;
 }
